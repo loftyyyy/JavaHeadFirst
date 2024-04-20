06d655e83d300211fb4790664a758df247bae3bf
loftyyyy <lofty08.business@gmail.com>
2024-04-20 11:11:41 +0800

    helo

---
+++ HeadFirstExercises/OOP/Animals/src/main/java/com/mycompany/app/Animals.java
@@ -0,0 +1,22 @@
+public class Animals{
+    private String picture;
+    private String food;
+    private String hunger;
+    private String boundaries;
+    private String location;
+
+    void MakeNoise(){
+
+    }
+
+    void eat(){
+
+    }
+
+    void sleep(){
+
+    }
+    void roam(){
+
+    }
+}
