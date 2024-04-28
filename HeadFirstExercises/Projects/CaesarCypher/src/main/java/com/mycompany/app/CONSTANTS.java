3aad11e8d0cbecf347e644855e39f00830879677
loftyyyy <lofty08.business@gmail.com>
2024-04-28 17:32:39 +0800

    Hi

--- HeadFirstExercises/Projects/CaesarCypher/src/main/java/com/mycompany/app/CONSTANTS.java
+++ HeadFirstExercises/Projects/CaesarCypher/src/main/java/com/mycompany/app/CONSTANTS.java
@@ -4,6 +4,4 @@ public abstract class CONSTANTS {
     public final static String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     public final static String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    public final static String[] ALPHABETCSV = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    public final static String[] alphabetcsv  = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" ,"t", "u", "v", "w", "x", "y", "z"};
 }
