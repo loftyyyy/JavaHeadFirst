f02997614b312c09b56bf138dbe6ca2e356c3f64
loftyyyy <lofty08.business@gmail.com>
2024-05-25 22:06:35 +0800

    Hi

--- HeadFirstExercises/Projects/SongBox/my-app/src/main/java/com/mycompany/app/ReviewJukeBox.java
+++ HeadFirstExercises/Projects/SongBox/my-app/src/main/java/com/mycompany/app/ReviewJukeBox.java
@@ -3,12 +3,16 @@ package com.mycompany.app;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import static java.util.Collections.sort;
 
 public class ReviewJukeBox {
 
     public static void main(String[] args){
+        String s1 = "hi";
+        String s2 = new String("hi");
+        System.out.println(s1.equals(s2));
         ArrayList<ReviewSongs> songs = ReviewMockUpSong2.getSongs();
         ReviewSortByTitle sortTitle = new ReviewSortByTitle();
 
@@ -19,7 +23,10 @@ public class ReviewJukeBox {
         System.out.println(songs);
 
         Set<ReviewSongs> songSet = new HashSet<>(songs);
+        Set<ReviewSongs> songTreeSet = new TreeSet<>((o1, o2) -> Integer.compare(o2.getBpm(), o1.getBpm()));
+        songTreeSet.addAll(songs);
 
+        System.out.println(songTreeSet);
         System.out.println(songSet);
 
 
